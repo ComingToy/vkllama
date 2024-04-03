f1d62918a8813f3cb524384745b53021c2b3a048
Conley Lee <conleylee@foxmail.com>
2024-04-03 19:34:15 +0800

    fix tensor assign op

--- src/core/tensor.cpp
+++ src/core/tensor.cpp
@@ -5,230 +5,237 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
 
-VkTensor::VkTensor()
-    : c_(0), h_(0), w_(0), dev_(nullptr), visable_(false), data_(VK_NULL_HANDLE), status_(nullptr)
+VkTensor::VkTensor ()
+    : c_ (0), h_ (0), w_ (0), dev_ (nullptr), visable_ (false),
+      data_ (VK_NULL_HANDLE), status_ (nullptr)
 {
-    mem_ = {VK_NULL_HANDLE, 0, 0, 0, nullptr, 0};
+  mem_ = { VK_NULL_HANDLE, 0, 0, 0, nullptr, 0 };
 }
-VkTensor::VkTensor(const int c,
-                   const int h,
-                   const int w,
-                   GPUDevice* dev,
-                   const bool visable)
-    : c_(c), h_(h), w_(w), dev_(dev), visable_(visable), data_(VK_NULL_HANDLE), status_(nullptr)
+VkTensor::VkTensor (const int c, const int h, const int w, GPUDevice *dev,
+                    const bool visable)
+    : c_ (c), h_ (h), w_ (w), dev_ (dev), visable_ (visable),
+      data_ (VK_NULL_HANDLE), status_ (nullptr)
 {
-    mem_ = {VK_NULL_HANDLE, 0, 0, 0, nullptr, 0};
+  mem_ = { VK_NULL_HANDLE, 0, 0, 0, nullptr, 0 };
 }
 
-VkTensor::VkTensor(const VkTensor& rhs)
-    : c_(rhs.channels()), h_(rhs.height()), w_(rhs.width()), dev_(rhs.dev_), visable_(rhs.visable()), data_(rhs.data_), mem_(rhs.mem_), status_(rhs.status_)
+VkTensor::VkTensor (const VkTensor &rhs)
+    : c_ (rhs.channels ()), h_ (rhs.height ()), w_ (rhs.width ()),
+      dev_ (rhs.dev_), visable_ (rhs.visable ()), data_ (rhs.data_),
+      mem_ (rhs.mem_), status_ (rhs.status_)
 {
-    if (status_)
+  if (status_)
     {
-        status_->ref_.fetch_add(1);
+      status_->ref_.fetch_add (1);
     }
 }
 
-VkTensor::VkTensor(VkTensor&& rhs)
-    : c_(rhs.channels()), h_(rhs.height()), w_(rhs.width()), dev_(rhs.dev_), visable_(rhs.visable_), data_(rhs.data_), mem_(rhs.mem_), status_(rhs.status_)
+VkTensor::VkTensor (VkTensor &&rhs)
+    : c_ (rhs.channels ()), h_ (rhs.height ()), w_ (rhs.width ()),
+      dev_ (rhs.dev_), visable_ (rhs.visable_), data_ (rhs.data_),
+      mem_ (rhs.mem_), status_ (rhs.status_)
 {
-    rhs.status_ = nullptr;
+  rhs.status_ = nullptr;
 }
 
-VkTensor&
-VkTensor::operator=(VkTensor const& rhs)
+VkTensor &
+VkTensor::operator= (VkTensor const &rhs)
 {
-    c_ = rhs.channels();
-    h_ = rhs.height();
-    w_ = rhs.width();
-    dev_ = rhs.dev_;
-    visable_ = rhs.visable_;
-    data_ = rhs.data_;
-    mem_ = rhs.mem_;
-    status_ = rhs.status_;
 
-    if (status_)
+  if (rhs.status_)
     {
-        status_->ref_.fetch_add(1);
+      rhs.status_->ref_.fetch_add (1);
     }
-    return *this;
+
+  release_ ();
+  c_ = rhs.channels ();
+  h_ = rhs.height ();
+  w_ = rhs.width ();
+  dev_ = rhs.dev_;
+  visable_ = rhs.visable_;
+  data_ = rhs.data_;
+  mem_ = rhs.mem_;
+  status_ = rhs.status_;
+
+  return *this;
 }
 
 VkResult
-VkTensor::create()
+VkTensor::create ()
 {
-    auto bytes = sizeof(float) * w_ * h_ * c_;
-    bytes = (bytes + 63) / 64 * 64;
-    {
-        VkBufferCreateInfo createInfo = {VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
-                                         nullptr,
-                                         0,
-                                         bytes,
-                                         VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                                         VK_SHARING_MODE_EXCLUSIVE,
-                                         0,
-                                         nullptr};
+  auto bytes = sizeof (float) * w_ * h_ * c_;
+  bytes = (bytes + 63) / 64 * 64;
+  {
+    VkBufferCreateInfo createInfo = { VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+                                      nullptr,
+                                      0,
+                                      bytes,
+                                      VK_BUFFER_USAGE_STORAGE_BUFFER_BIT
+                                          | VK_BUFFER_USAGE_TRANSFER_DST_BIT
+                                          | VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                                      VK_SHARING_MODE_EXCLUSIVE,
+                                      0,
+                                      nullptr };
 
-        auto ret = vkCreateBuffer(dev_->device(), &createInfo, nullptr, &data_);
-        if (ret != VK_SUCCESS)
-            return ret;
-    }
-
-    VkMemoryRequirements req;
-    vkGetBufferMemoryRequirements(dev_->device(), data_, &req);
-
-    auto ret = dev_->allocator().allocate(req, &mem_, visable_);
+    auto ret = vkCreateBuffer (dev_->device (), &createInfo, nullptr, &data_);
     if (ret != VK_SUCCESS)
+      return ret;
+  }
+
+  VkMemoryRequirements req;
+  vkGetBufferMemoryRequirements (dev_->device (), data_, &req);
+
+  auto ret = dev_->allocator ().allocate (req, &mem_, visable_);
+  if (ret != VK_SUCCESS)
     {
-        vkDestroyBuffer(dev_->device(), data_, nullptr);
-        data_ = VK_NULL_HANDLE;
-        return ret;
+      vkDestroyBuffer (dev_->device (), data_, nullptr);
+      data_ = VK_NULL_HANDLE;
+      return ret;
     }
 
-    ret = vkBindBufferMemory(dev_->device(), data_, mem_.mem, mem_.offset);
-    if (ret != VK_SUCCESS)
+  ret = vkBindBufferMemory (dev_->device (), data_, mem_.mem, mem_.offset);
+  if (ret != VK_SUCCESS)
     {
-        vkDestroyBuffer(dev_->device(), data_, nullptr);
-        dev_->allocator().free(mem_);
-        data_ = VK_NULL_HANDLE;
-        return ret;
+      vkDestroyBuffer (dev_->device (), data_, nullptr);
+      dev_->allocator ().free (mem_);
+      data_ = VK_NULL_HANDLE;
+      return ret;
     }
 
-    status_ = new __TensorStatus();
-    status_->access_flags_.store(0);
-    status_->pipeline_stage_.store(0);
-    status_->ref_.store(1);
-    return VK_SUCCESS;
+  status_ = new __TensorStatus ();
+  status_->access_flags_.store (0);
+  status_->pipeline_stage_.store (0);
+  status_->ref_.store (1);
+  return VK_SUCCESS;
 }
 
 size_t
-VkTensor::bytes() const
+VkTensor::bytes () const
 {
-    auto bytes = sizeof(float) * w_ * h_ * c_;
-    bytes = (bytes + 63) / 64 * 64;
-    return bytes;
+  auto bytes = sizeof (float) * w_ * h_ * c_;
+  bytes = (bytes + 63) / 64 * 64;
+  return bytes;
 }
 
-void* VkTensor::host()
+void *
+VkTensor::host ()
 {
-    return mem_.host;
+  return mem_.host;
 }
 
 VkAccessFlags
-VkTensor::access_flags() const
+VkTensor::access_flags () const
 {
-    return status_ ? status_->access_flags_.load() : 0;
+  return status_ ? status_->access_flags_.load () : 0;
 }
 
 VkPipelineStageFlags
-VkTensor::pipeline_stage() const
+VkTensor::pipeline_stage () const
 {
-    return status_ ? status_->pipeline_stage_.load() : 0;
+  return status_ ? status_->pipeline_stage_.load () : 0;
 }
 
-void VkTensor::set_access_flags(VkAccessFlags flags)
+void
+VkTensor::set_access_flags (VkAccessFlags flags)
 {
-    if (!status_)
-        return;
-    status_->access_flags_.store(flags);
+  if (!status_)
+    return;
+  status_->access_flags_.store (flags);
 }
 
-void VkTensor::set_pipeline_stage(VkPipelineStageFlags stage)
+void
+VkTensor::set_pipeline_stage (VkPipelineStageFlags stage)
 {
-    if (!status_)
-        return;
-    status_->pipeline_stage_.store(stage);
+  if (!status_)
+    return;
+  status_->pipeline_stage_.store (stage);
 }
 
-bool VkTensor::visable() const
+bool
+VkTensor::visable () const
 {
-    return visable_;
-}
-
-size_t
-VkTensor::channels() const
-{
-    return c_;
+  return visable_;
 }
 
 size_t
-VkTensor::height() const
+VkTensor::channels () const
 {
-    return h_;
+  return c_;
 }
 
 size_t
-VkTensor::width() const
+VkTensor::height () const
 {
-    return w_;
+  return h_;
 }
 
-size_t VkTensor::size() const
+size_t
+VkTensor::width () const
 {
-    return c_ * h_ * w_;
+  return w_;
 }
 
-VkBuffer&
-VkTensor::data()
+size_t
+VkTensor::size () const
 {
-    return data_;
+  return c_ * h_ * w_;
+}
+
+VkBuffer &
+VkTensor::data ()
+{
+  return data_;
 }
 
 VkResult
-VkTensor::flush()
+VkTensor::flush ()
 {
-    if (!visable_)
+  if (!visable_)
     {
-        return VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS;
+      return VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS;
     }
 
-    VkMappedMemoryRange range = {VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE,
-                                 nullptr,
-                                 mem_.mem,
-                                 mem_.offset,
-                                 mem_.size};
+  VkMappedMemoryRange range = { VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE, nullptr,
+                                mem_.mem, mem_.offset, mem_.size };
 
-    return vkFlushMappedMemoryRanges(dev_->device(), 1, &range);
+  return vkFlushMappedMemoryRanges (dev_->device (), 1, &range);
 }
 
 VkResult
-VkTensor::invalid()
+VkTensor::invalid ()
 {
-    if (!visable_)
+  if (!visable_)
     {
-        return VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS;
+      return VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS;
     }
 
-    VkMappedMemoryRange range = {VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE,
-                                 nullptr,
-                                 mem_.mem,
-                                 mem_.offset,
-                                 mem_.size};
+  VkMappedMemoryRange range = { VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE, nullptr,
+                                mem_.mem, mem_.offset, mem_.size };
 
-    return vkInvalidateMappedMemoryRanges(dev_->device(), 1, &range);
+  return vkInvalidateMappedMemoryRanges (dev_->device (), 1, &range);
 }
 
-VkTensor::~VkTensor()
+void
+VkTensor::release_ ()
 {
-    if (status_ && status_->ref_.fetch_sub(1) == 1)
+  if (status_ && status_->ref_.fetch_sub (1) == 1)
     {
-        if (data_ != VK_NULL_HANDLE)
+      if (data_ != VK_NULL_HANDLE)
         {
-            dev_->allocator().free(mem_);
-            vkDestroyBuffer(dev_->device(), data_, nullptr);
+          dev_->allocator ().free (mem_);
+          vkDestroyBuffer (dev_->device (), data_, nullptr);
         }
 
-        delete status_;
+      delete status_;
     }
 }
 
+VkTensor::~VkTensor () { release_ (); }
+
 VkTensor
-VkTensor::like(const VkTensor& tensor)
+VkTensor::like (const VkTensor &tensor)
 {
-    VkTensor tmp(tensor.channels(),
-                 tensor.height(),
-                 tensor.width(),
-                 tensor.dev_,
-                 tensor.visable());
-    return tmp;
+  VkTensor tmp (tensor.channels (), tensor.height (), tensor.width (),
+                tensor.dev_, tensor.visable ());
+  return tmp;
 }
--- src/core/tensor.h
+++ src/core/tensor.h
@@ -9,62 +9,60 @@ class GPUDevice;
 class VkTensor
 {
 public:
-    typedef enum
-    {
-        FP32
-    } DType;
+  typedef enum
+  {
+    FP32
+  } DType;
 
-    static VkTensor like(VkTensor const&);
-    VkTensor();
-    VkTensor(const int c,
-             const int h,
-             const int w,
-             GPUDevice* dev,
-             const bool visable = false);
+  static VkTensor like (VkTensor const &);
+  VkTensor ();
+  VkTensor (const int c, const int h, const int w, GPUDevice *dev,
+            const bool visable = false);
 
-    VkTensor& operator=(VkTensor const&);
-    VkTensor(const VkTensor& rhs);
-    VkTensor(VkTensor&& rhs);
+  VkTensor &operator= (VkTensor const &);
+  VkTensor (const VkTensor &rhs);
+  VkTensor (VkTensor &&rhs);
 
-    ~VkTensor();
+  ~VkTensor ();
 
-    size_t channels() const;
-    size_t height() const;
-    size_t width() const;
-    size_t size() const;
-    VkAccessFlags access_flags() const;
-    VkPipelineStageFlags pipeline_stage() const;
-    void set_access_flags(VkAccessFlags access_flags);
-    void set_pipeline_stage(VkPipelineStageFlags stage_flags);
+  size_t channels () const;
+  size_t height () const;
+  size_t width () const;
+  size_t size () const;
+  VkAccessFlags access_flags () const;
+  VkPipelineStageFlags pipeline_stage () const;
+  void set_access_flags (VkAccessFlags access_flags);
+  void set_pipeline_stage (VkPipelineStageFlags stage_flags);
 
-    VkBuffer& data();
+  VkBuffer &data ();
 
-    VkResult create();
-    size_t bytes() const;
-    bool visable() const;
-    VkResult flush();
-    VkResult invalid();
+  VkResult create ();
+  size_t bytes () const;
+  bool visable () const;
+  VkResult flush ();
+  VkResult invalid ();
 
-    void* host();
+  void *host ();
 
 private:
-    int c_;
-    int h_;
-    int w_;
-    size_t bytes_;
+  int c_;
+  int h_;
+  int w_;
+  size_t bytes_;
 
-    GPUDevice* dev_;
-    bool visable_;
-    VkBuffer data_;
-    Allocator::MemBlock mem_;
-    struct __TensorStatus
-    {
-        std::atomic<VkAccessFlags> access_flags_;
-        std::atomic<VkPipelineStageFlags> pipeline_stage_;
-        std::atomic<int> ref_;
-    };
+  GPUDevice *dev_;
+  bool visable_;
+  VkBuffer data_;
+  Allocator::MemBlock mem_;
+  struct __TensorStatus
+  {
+    std::atomic<VkAccessFlags> access_flags_;
+    std::atomic<VkPipelineStageFlags> pipeline_stage_;
+    std::atomic<int> ref_;
+  };
 
-    __TensorStatus* status_;
+  __TensorStatus *status_;
+  void release_ ();
 };
 
 #endif
