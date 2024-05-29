84e68e31f6b007c7d04bbfb6ce9fa5aaf0699c8a
Conley Lee <conleylee@foxmail.com>
2024-05-29 21:24:52 +0800

    add rmsnorm fp16

--- tests/test_rmsnorm.cpp
+++ tests/test_rmsnorm.cpp
@@ -1,6 +1,7 @@
 #include "Eigen/Eigen"
 #include "core/command.h"
 #include "core/gpu_device.h"
+#include "ops/cast.h"
 #include "ops/rms_norm.h"
 #include "test_common.h"
 #include "gtest/gtest.h"
@@ -14,6 +15,7 @@ struct TestRMSNormParams
   const int C;
   const int H;
   const int W;
+  const int dtype;
 };
 
 using TestRMSNorm = VkllamaTestWithParam<TestRMSNormParams>;
@@ -30,15 +32,67 @@ TEST_P (TestRMSNorm, test_rmsnorm)
   ASSERT_TRUE (input0);
   ASSERT_TRUE (input1);
 
-  RMSNorm norm_op (gpu_, command_, input1->first);
+  VkTensor input0_fp16, input0_fp32, input1_fp16, input1_fp32;
+  std::vector<float> input0_buf, input1_buf;
+
+  Cast cast_input0_fp16 (gpu_, command_, VkTensor::FP32, VkTensor::FP16),
+      cast_input0_fp32 (gpu_, command_, VkTensor::FP16, VkTensor::FP32),
+      cast_input1_fp16 (gpu_, command_, VkTensor::FP32, VkTensor::FP16),
+      cast_input1_fp32 (gpu_, command_, VkTensor::FP16, VkTensor::FP32);
+  if (params.dtype)
+    {
+      ASSERT_EQ (cast_input0_fp16.init (), VK_SUCCESS);
+      ASSERT_EQ (cast_input0_fp32.init (), VK_SUCCESS);
+      ASSERT_EQ (cast_input1_fp16.init (), VK_SUCCESS);
+      ASSERT_EQ (cast_input1_fp32.init (), VK_SUCCESS);
+
+      ASSERT_EQ (cast_input0_fp16 (input0->first, input0_fp16), VK_SUCCESS);
+      ASSERT_EQ (cast_input0_fp32 (input0_fp16, input0_fp32), VK_SUCCESS);
+      ASSERT_EQ (cast_input1_fp16 (input1->first, input1_fp16), VK_SUCCESS);
+      ASSERT_EQ (cast_input1_fp32 (input1_fp16, input1_fp32), VK_SUCCESS);
+
+      input0_buf.resize (input0_fp32.size ());
+      input1_buf.resize (input1_fp32.size ());
+      ASSERT_EQ (command_->download (input0_fp32, input0_buf.data (),
+                                     input0_buf.size ()),
+                 VK_SUCCESS);
+
+      ASSERT_EQ (command_->download (input1_fp32, input1_buf.data (),
+                                     input1_buf.size ()),
+                 VK_SUCCESS);
+    }
+  else
+    {
+      input0_fp32 = input0->first;
+      input1_fp32 = input1->first;
+      input0_buf.swap (input0->second);
+      input1_buf.swap (input1->second);
+    }
+
+  RMSNorm norm_op (gpu_, command_, params.dtype ? input1_fp16 : input1_fp32,
+                   1e-3f, (VkTensor::DType)params.dtype);
   ASSERT_EQ (norm_op.init (), VK_SUCCESS);
 
   VkTensor output;
-  ASSERT_EQ (norm_op (input0->first, output), VK_SUCCESS);
+  ASSERT_EQ (norm_op (params.dtype ? input0_fp16 : input0_fp32, output),
+             VK_SUCCESS);
+
+  VkTensor output_fp32;
+  Cast cast_output_op (gpu_, command_, VkTensor::FP16, VkTensor::FP32);
+  if (params.dtype)
+    {
+      ASSERT_EQ (cast_output_op.init (), VK_SUCCESS);
+      ASSERT_EQ (cast_output_op (output, output_fp32), VK_SUCCESS);
+    }
+  else
+    {
+      output_fp32 = output;
+    }
+
   std::vector<float> output_buf (output.size ());
 
   ASSERT_EQ (
-      command_->download (output, output_buf.data (), output_buf.size ()),
+      command_->download (output_fp32, output_buf.data (), output_buf.size ()),
       VK_SUCCESS);
 
   ASSERT_EQ (command_->end (), VK_SUCCESS) << "failed at end commands";
@@ -50,12 +104,12 @@ TEST_P (TestRMSNorm, test_rmsnorm)
       (Eigen::Index)output.height (), (Eigen::Index)output.width ());
 
   Tensor<3> input_tensor0 = TensorMap<3> (
-      input0->second.data (), (Eigen::Index)input0->first.channels (),
+      input0_buf.data (), (Eigen::Index)input0->first.channels (),
       (Eigen::Index)input0->first.height (),
       (Eigen::Index)input0->first.width ());
 
   Tensor<3> input_tensor1 = TensorMap<3> (
-      input1->second.data (), (Eigen::Index)input1->first.channels (),
+      input1_buf.data (), (Eigen::Index)input1->first.channels (),
       (Eigen::Index)input1->first.height (),
       (Eigen::Index)input1->first.width ());
 
@@ -86,7 +140,11 @@ TEST_P (TestRMSNorm, test_rmsnorm)
   ASSERT_EQ (*diff.data (), 0);
 };
 
-std::vector<TestRMSNormParams> params = { { 1, 1023, 63 }, { 3, 1023, 63 } };
+std::vector<TestRMSNormParams> params = { { 1, 1023, 63, 1 },
+                                          { 3, 1023, 63, 1 },
+                                          { 1, 1023, 63, 0 },
+                                          { 3, 1023, 63, 0 } };
+
 INSTANTIATE_TEST_SUITE_P (test_rmsnorm, TestRMSNorm,
                           ::testing::ValuesIn (params));
 }
