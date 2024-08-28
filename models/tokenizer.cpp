3517206d6226985a77b832670071be044d04e204
conley <lika52090@gmail.com>
2024-08-28 19:02:15 +0800

    load sp from gguf

--- models/tokenizer.cpp
+++ models/tokenizer.cpp
@@ -1,7 +1,39 @@
 #include "tokenizer.h"
-sentencepiece::util::Status
-load_tokenizer (sentencepiece::SentencePieceProcessor &sp,
-                std::map<std::string, gguf_key> const &gguf_kv)
+#include <cstdio>
+
+#if 0
+	{LLM_KV_TOKENIZER_MODEL, "tokenizer.ggml.model"},
+    {LLM_KV_TOKENIZER_PRE, "tokenizer.ggml.pre"},
+    {LLM_KV_TOKENIZER_LIST, "tokenizer.ggml.tokens"},
+    {LLM_KV_TOKENIZER_TOKEN_TYPE, "tokenizer.ggml.token_type"},
+    {LLM_KV_TOKENIZER_TOKEN_TYPE_COUNT, "tokenizer.ggml.token_type_count"},
+    {LLM_KV_TOKENIZER_SCORES, "tokenizer.ggml.scores"},
+    {LLM_KV_TOKENIZER_MERGES, "tokenizer.ggml.merges"},
+    {LLM_KV_TOKENIZER_BOS_ID, "tokenizer.ggml.bos_token_id"},
+    {LLM_KV_TOKENIZER_EOS_ID, "tokenizer.ggml.eos_token_id"},
+    {LLM_KV_TOKENIZER_UNK_ID, "tokenizer.ggml.unknown_token_id"},
+    {LLM_KV_TOKENIZER_SEP_ID, "tokenizer.ggml.seperator_token_id"},
+    {LLM_KV_TOKENIZER_PAD_ID, "tokenizer.ggml.padding_token_id"},
+    {LLM_KV_TOKENIZER_CLS_ID, "tokenizer.ggml.cls_token_id"},
+    {LLM_KV_TOKENIZER_MASK_ID, "tokenizer.ggml.mask_token_id"},
+    {LLM_KV_TOKENIZER_ADD_BOS, "tokenizer.ggml.add_bos_token"},
+    {LLM_KV_TOKENIZER_ADD_EOS, "tokenizer.ggml.add_eos_token"},
+    {LLM_KV_TOKENIZER_ADD_PREFIX, "tokenizer.ggml.add_space_prefix"},
+    {LLM_KV_TOKENIZER_REMOVE_EXTRA_WS,
+     "tokenizer.ggml.remove_extra_whitespaces"},
+    {LLM_KV_TOKENIZER_PRECOMPILED_CHARSMAP,
+     "tokenizer.ggml.precompiled_charsmap"},
+    {LLM_KV_TOKENIZER_HF_JSON, "tokenizer.huggingface.json"},
+    {LLM_KV_TOKENIZER_RWKV, "tokenizer.rwkv.world"},
+    {LLM_KV_TOKENIZER_PREFIX_ID, "tokenizer.ggml.prefix_token_id"},
+    {LLM_KV_TOKENIZER_SUFFIX_ID, "tokenizer.ggml.suffix_token_id"},
+    {LLM_KV_TOKENIZER_MIDDLE_ID, "tokenizer.ggml.middle_token_id"},
+    {LLM_KV_TOKENIZER_EOT_ID, "tokenizer.ggml.eot_token_id"},
+#endif
+
+static sentencepiece::util::Status
+load_tokenizer_pieces (std::map<std::string, gguf_key> const &gguf_kv,
+                       sentencepiece::ModelProto &model)
 {
   if (!gguf_kv.count ("tokenizer.ggml.tokens"))
     {
@@ -113,7 +145,6 @@ load_tokenizer (sentencepiece::SentencePieceProcessor &sp,
           "tokenizer.ggml.scores != tokenizer.ggml.token_type");
     }
 
-  sentencepiece::ModelProto model;
   for (size_t i = 0; i < scores.size (); ++i)
     {
       auto p = model.add_pieces ();
@@ -122,9 +153,56 @@ load_tokenizer (sentencepiece::SentencePieceProcessor &sp,
       p->set_type ((::sentencepiece::ModelProto_SentencePiece_Type)types[i]);
     }
 
+  return sentencepiece::util::Status ();
+}
+
+static sentencepiece::util::Status
+load_trainer_spec (std::map<std::string, gguf_key> const &gguf_kv,
+                   sentencepiece::ModelProto &model)
+{
+  std::string tokenizer_model = "llama";
+  if (gguf_kv.count ("tokenizer.ggml.model") > 0)
+    {
+      auto tm = gguf_kv.find ("tokenizer.ggml.model")->second;
+      if (tm.type == GGUF_VALUE_TYPE_STRING)
+        tokenizer_model
+            = std::string (tm.val->string.string, tm.val->string.len);
+    }
+
+  std::string tokenizer_pre = "default";
+  if (gguf_kv.count ("tokenizer.ggml.pre") > 0)
+    {
+      auto pre = gguf_kv.find ("tokenizer.ggml.pre")->second;
+      if (pre.type == GGUF_VALUE_TYPE_STRING)
+        tokenizer_pre
+            = std::string (pre.val->string.string, pre.val->string.len);
+    }
+
   auto trainer_spec = model.mutable_trainer_spec ();
-  trainer_spec->set_model_type (sentencepiece::TrainerSpec_ModelType_BPE);
   trainer_spec->set_byte_fallback (true);
+  if (tokenizer_model == "llama")
+    {
+      trainer_spec->set_model_type (sentencepiece::TrainerSpec_ModelType_BPE);
+    }
+
+  return sentencepiece::util::Status ();
+}
+
+sentencepiece::util::Status
+load_tokenizer (sentencepiece::SentencePieceProcessor &sp,
+                std::map<std::string, gguf_key> const &gguf_kv)
+{
+
+  sentencepiece::ModelProto model;
+  if (auto s = load_tokenizer_pieces (gguf_kv, model); !s.ok ())
+    {
+      return s;
+    }
+
+  if (auto s = load_trainer_spec (gguf_kv, model); !s.ok ())
+    {
+      return s;
+    }
 
   return sp.Load (model);
 }
