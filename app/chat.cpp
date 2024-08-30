504402221ccaa41dbdfce3d31296a9ccc815fca8
conley <lika52090@gmail.com>
2024-08-30 17:51:22 +0800

    parse cmdline

--- app/chat.cpp
+++ app/chat.cpp
@@ -5,7 +5,7 @@
 
 struct Params
 {
-  const char *gguf_file;
+  const char *model_file;
   const char *prompt_template;
   std::vector<const char *> anti_prompt;
   const char *sampler;
@@ -26,7 +26,7 @@ show_usage (int argc, char *const argv[])
 _H (NAME)"\n"
 "    chat - chat with llama2 interactive \n\n"
 _H (SYNOPSIS)"\n"
-"    chat " _H(-m) " path " _H(-t) " path " "[" _H(-s) " {top_k|top_p}] [" _H(-k) " value] " "[" _H(-p) " value]" "\n"
+"    chat " _H(-m) " path " _H(-t) " path [" _H(-s) " {top_k|top_p}] [" _H(-k) " value] " "[" _H(-p) " value]" "\n"
 "\n"
 _H(DESCRIPTION)"\n"
 "    the options are follow:\n"
@@ -34,7 +34,7 @@ _H(DESCRIPTION)"\n"
 "    " _H(-t) "\tpath to the prompt template file\n"
 "    " _H(-s) "\tsampler. top_k or top_p are supported. (default: top_k)\n"
 "    " _H(-k) "\tthe k option of top_k sampler. (default: 40)\n"
-"    " _H(-p) "\tthe p option of top_k sampler. (default: 0.75)\n"
+"    " _H(-p) "\tthe p option of top_p sampler. (default: 0.75)\n"
 ;
   // clang-format on
   fprintf (stdout, fmt);
@@ -49,7 +49,7 @@ parse_params_from_cmdline (int argc, char *const argv[], Params *params)
       switch (ch)
         {
         case 'm':
-          params->gguf_file = optarg;
+          params->model_file = optarg;
         case 't':
           params->prompt_template = optarg;
           break;
@@ -81,7 +81,8 @@ main (int argc, char *const argv[])
   int ret = 0;
 
   show_usage (argc, argv);
-  Params params = { .prompt_template = nullptr,
+  Params params = { .model_file = nullptr,
+                    .prompt_template = nullptr,
                     .anti_prompt = {},
                     .sampler = "topk",
                     .sampler_option = { .topk = 40, .p = 0.75 } };
@@ -89,5 +90,15 @@ main (int argc, char *const argv[])
   if ((ret = parse_params_from_cmdline (argc, argv, &params)) != 0)
     {
       return ret;
+    }
+
+  fprintf (
+      stderr,
+      "model_file: %s\nprompt_template: %s\nsampler: %s\ntopk: %d\np:%f\n",
+      params.model_file, params.prompt_template, params.sampler,
+      params.sampler_option.topk, params.sampler_option.p);
+  for (size_t i = 0; i < params.anti_prompt.size (); ++i)
+    {
+      fprintf (stderr, "anti_prompt[%zu]: %s\n", i, params.anti_prompt[i]);
     }
 }
