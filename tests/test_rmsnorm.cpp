e7dc407105cadd7745693f5872ac4e75bcc8f20b
Conley Lee <conleylee@foxmail.com>
2024-09-11 20:57:23 +0800

    fix test_rmsnorm

--- tests/test_rmsnorm.cpp
+++ tests/test_rmsnorm.cpp
@@ -26,96 +26,42 @@ TEST_P (TestRMSNorm, test_rmsnorm)
       << "failed at begin commands";
 
   auto params = GetParam ();
-  auto input0
-      = random_tensor<float> (gpu_, command_, params.C, params.H, params.W);
-  auto input1 = random_tensor<float> (gpu_, command_, 1, 1, params.W);
+  auto input0 = random_tensor<Eigen::half> (gpu_, command_, params.C, params.H,
+                                            params.W);
+  auto input1 = random_tensor<Eigen::half> (gpu_, command_, 1, 1, params.W);
 
   ASSERT_TRUE (input0);
   ASSERT_TRUE (input1);
 
-  Tensor input0_fp16, input0_fp32, input1_fp16, input1_fp32;
-  std::vector<float> input0_buf, input1_buf;
-
-  Cast cast_input0_fp16 (gpu_, command_, Tensor::FP32, Tensor::FP16),
-      cast_input0_fp32 (gpu_, command_, Tensor::FP16, Tensor::FP32),
-      cast_input1_fp16 (gpu_, command_, Tensor::FP32, Tensor::FP16),
-      cast_input1_fp32 (gpu_, command_, Tensor::FP16, Tensor::FP32);
-  if (params.dtype)
-    {
-      ASSERT_EQ (cast_input0_fp16.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_input0_fp32.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_input1_fp16.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_input1_fp32.init (), absl::OkStatus ());
-
-      ASSERT_EQ (cast_input0_fp16 (input0->first, input0_fp16),
-                 absl::OkStatus ());
-      ASSERT_EQ (cast_input0_fp32 (input0_fp16, input0_fp32),
-                 absl::OkStatus ());
-      ASSERT_EQ (cast_input1_fp16 (input1->first, input1_fp16),
-                 absl::OkStatus ());
-      ASSERT_EQ (cast_input1_fp32 (input1_fp16, input1_fp32),
-                 absl::OkStatus ());
-
-      input0_buf.resize (input0_fp32.size ());
-      input1_buf.resize (input1_fp32.size ());
-      ASSERT_EQ (command_->download (input0_fp32, input0_buf.data (),
-                                     input0_buf.size ()),
-                 absl::OkStatus ());
-
-      ASSERT_EQ (command_->download (input1_fp32, input1_buf.data (),
-                                     input1_buf.size ()),
-                 absl::OkStatus ());
-    }
-  else
-    {
-      input0_fp32 = input0->first;
-      input1_fp32 = input1->first;
-      input0_buf.swap (input0->second);
-      input1_buf.swap (input1->second);
-    }
-
-  RMSNorm norm_op (gpu_, command_, params.dtype ? input1_fp16 : input1_fp32,
-                   1e-3f, (Tensor::DType)params.dtype);
+  RMSNorm norm_op (gpu_, command_, input1->first, 1e-3f,
+                   (Tensor::DType)params.dtype);
   ASSERT_EQ (norm_op.init (), absl::OkStatus ());
 
-  Tensor output;
-  ASSERT_EQ (norm_op (params.dtype ? input0_fp16 : input0_fp32, output),
+  absl::StatusOr<Tensor> output;
+  ASSERT_EQ ((output = norm_op (input0->first)).status (), absl::OkStatus ());
+
+  std::vector<Eigen::half> output_buf (output->size ());
+
+  ASSERT_EQ (command_->download (*output,
+                                 (__vkllama_fp16_t *)output_buf.data (),
+                                 output_buf.size ()),
              absl::OkStatus ());
-
-  Tensor output_fp32;
-  Cast cast_output_op (gpu_, command_, Tensor::FP16, Tensor::FP32);
-  if (params.dtype)
-    {
-      ASSERT_EQ (cast_output_op.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_output_op (output, output_fp32), absl::OkStatus ());
-    }
-  else
-    {
-      output_fp32 = output;
-    }
-
-  std::vector<float> output_buf (output.size ());
-  std::cerr << "size of output buf: " << output_buf.size () << std::endl;
-
-  ASSERT_EQ (
-      command_->download (output_fp32, output_buf.data (), output_buf.size ()),
-      absl::OkStatus ());
 
   ASSERT_EQ (command_->end (), absl::OkStatus ()) << "failed at end commands";
   ASSERT_EQ (command_->submit_and_wait (), absl::OkStatus ())
       << "failed at submit commands";
 
-  _Tensor<float, 3> vk_output_tensor = TensorMap<3> (
-      output_buf.data (), (Eigen::Index)output.channels (),
-      (Eigen::Index)output.height (), (Eigen::Index)output.width ());
+  _Tensor<Eigen::half, 3> vk_output_tensor = _TensorMap<Eigen::half, 3> (
+      output_buf.data (), (Eigen::Index)output->channels (),
+      (Eigen::Index)output->height (), (Eigen::Index)output->width ());
 
-  _Tensor<float, 3> input_tensor0 = TensorMap<3> (
-      input0_buf.data (), (Eigen::Index)input0->first.channels (),
+  _Tensor<Eigen::half, 3> input_tensor0 = _TensorMap<Eigen::half, 3> (
+      input0->second.data (), (Eigen::Index)input0->first.channels (),
       (Eigen::Index)input0->first.height (),
       (Eigen::Index)input0->first.width ());
 
-  _Tensor<float, 3> input_tensor1 = TensorMap<3> (
-      input1_buf.data (), (Eigen::Index)input1->first.channels (),
+  _Tensor<Eigen::half, 3> input_tensor1 = _TensorMap<Eigen::half, 3> (
+      input1->second.data (), (Eigen::Index)input1->first.channels (),
       (Eigen::Index)input1->first.height (),
       (Eigen::Index)input1->first.width ());
 
@@ -127,8 +73,9 @@ TEST_P (TestRMSNorm, test_rmsnorm)
   Eigen::array<Eigen::Index, 3> weight_broadcasts
       = { input_tensor0.dimension (0), input_tensor0.dimension (1), 1 };
 
-  _Tensor<float, 3> eigen_output_tensor
-      = (input_tensor0.pow (2.0f).mean (mean_dims) + 1e-3f)
+  _Tensor<Eigen::half, 3> eigen_output_tensor
+      = (input_tensor0.pow (Eigen::half (2.0f)).mean (mean_dims)
+         + Eigen::half (1e-3f))
             .rsqrt ()
             .reshape (dims)
             .broadcast (broadcasts)
@@ -136,8 +83,9 @@ TEST_P (TestRMSNorm, test_rmsnorm)
   // std::cerr << "input tensor: " << input_tensor0 << std::endl
   //           << "vk output tensor: " << vk_output_tensor << std::endl
   //           << "eigen output tensor: " << eigen_output_tensor << std::endl;
-  _Tensor<float, 3> err (vk_output_tensor.dimensions ());
-  err.setConstant (1e-3);
+  _Tensor<Eigen::half, 3> err (vk_output_tensor.dimensions ());
+  err.setConstant (Eigen::half (1e-2));
+
   _Tensor<int, 0> diff
       = ((vk_output_tensor - eigen_output_tensor).abs () > err)
             .cast<int> ()
