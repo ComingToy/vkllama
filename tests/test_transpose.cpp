2a1a447f7ba9ffeadc5cced1de4088be4516004f
Conley Lee <conleylee@foxmail.com>
2024-09-11 21:27:04 +0800

    fix test_transpose

--- tests/test_transpose.cpp
+++ tests/test_transpose.cpp
@@ -47,100 +47,48 @@ TEST_P (TestTranspose, test_transpose)
 {
   ASSERT_EQ (command_->begin (), absl::OkStatus ());
   auto params = GetParam ();
-  auto input0
-      = random_tensor<float> (gpu_, command_, params.C, params.H, params.W);
-
-  Tensor input_fp32, input_fp16;
-  std::vector<float> input_buf;
-  Cast cast_input_op0 (gpu_, command_, Tensor::FP32, Tensor::FP16);
-  Cast cast_input_op1 (gpu_, command_, Tensor::FP16, Tensor::FP32);
-  if (params.dtype)
-    {
-      ASSERT_EQ (cast_input_op0.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_input_op1.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_input_op0 (input0->first, input_fp16),
-                 absl::OkStatus ());
-      ASSERT_EQ (cast_input_op1 (input_fp16, input_fp32), absl::OkStatus ());
-
-      input_buf.resize (input_fp16.size ());
-      ASSERT_EQ (command_->download (input_fp32, input_buf.data (),
-                                     input_buf.size ()),
-                 absl::OkStatus ());
-    }
-  else
-    {
-      input_fp32 = input0->first;
-      input_buf.swap (input0->second);
-    }
+  auto input0 = random_tensor<Eigen::half> (gpu_, command_, params.C, params.H,
+                                            params.W);
 
   Transpose transpose_op (gpu_, command_, 0, (Tensor::DType)params.dtype);
   ASSERT_EQ (transpose_op.init (), absl::OkStatus ());
 
-  Tensor output;
-  if (params.dtype)
-    {
-      transpose_op (input_fp16, output);
-    }
-  else
-    {
-      transpose_op (input_fp32, output);
-    }
+  absl::StatusOr<Tensor> output;
+  ASSERT_TRUE ((output = transpose_op (input0->first)).ok ())
+      << output.status ();
 
-  std::vector<float> output_buf (output.size ());
-  Tensor output_fp32;
-  Cast cast_output_op (gpu_, command_, Tensor::FP16, Tensor::FP32);
-  if (params.dtype)
-    {
-      ASSERT_EQ (cast_output_op.init (), absl::OkStatus ());
-      ASSERT_EQ (cast_output_op (output, output_fp32), absl::OkStatus ());
-    }
-  else
-    {
-      output_fp32 = output;
-    }
+  std::vector<Eigen::half> output_buf (output->size ());
 
   ASSERT_EQ (
-      command_->download (output_fp32, output_buf.data (), output_buf.size ()),
+      command_->download (*output, output_buf.data (), output_buf.size ()),
       absl::OkStatus ());
   ASSERT_EQ (command_->end (), absl::OkStatus ());
   ASSERT_EQ (command_->submit_and_wait (), absl::OkStatus ());
 
-  _Tensor<float, 3> input_tensor
-      = TensorMap<3> (input_buf.data (), (Eigen::Index)params.C,
-                      (Eigen::Index)params.H, (Eigen::Index)params.W);
+  _Tensor<Eigen::half, 3> input_tensor = _TensorMap<Eigen::half, 3> (
+      input0->second.data (), (Eigen::Index)params.C, (Eigen::Index)params.H,
+      (Eigen::Index)params.W);
 
   Eigen::array<Eigen::Index, 3> shuffle = { 1, 0, 2 };
 
-  _Tensor<float, 3> vk_output_tensor = TensorMap<3> (
-      output_buf.data (), (Eigen::Index)output_fp32.channels (),
-      (Eigen::Index)output_fp32.height (), (Eigen::Index)output_fp32.width ());
+  _Tensor<Eigen::half, 3> vk_output_tensor = _TensorMap<Eigen::half, 3> (
+      output_buf.data (), (Eigen::Index)output->channels (),
+      (Eigen::Index)output->height (), (Eigen::Index)output->width ());
 
-  _Tensor<float, 3> err (vk_output_tensor.dimensions ());
-  err.setConstant (params.dtype ? 1e-2 : 1e-3);
+  _Tensor<Eigen::half, 3> err (vk_output_tensor.dimensions ());
+  err.setConstant (Eigen::half (params.dtype ? 1e-2 : 1e-3));
 
-  if (params.dtype)
-    {
-      auto transposed
-          = input_tensor.cast<Eigen::half> ().shuffle (shuffle).cast<float> ();
-      _Tensor<int, 0> diff
-          = ((vk_output_tensor - transposed).abs () > err).cast<int> ().sum ();
+  _Tensor<Eigen::half, 3> transposed = input_tensor.shuffle (shuffle);
 
-      ASSERT_EQ (*diff.data (), 0);
-    }
-  else
-    {
-      auto transposed = input_tensor.shuffle (shuffle);
-      _Tensor<int, 0> diff
-          = ((vk_output_tensor - transposed).abs () > err).cast<int> ().sum ();
+  _Tensor<int, 0> diff
+      = ((vk_output_tensor - transposed).abs () > err).cast<int> ().sum ();
 
-      ASSERT_EQ (*diff.data (), 0);
-    }
+  ASSERT_EQ (*diff.data (), 0);
 }
 
-std::vector<TestTransposeParams> params
-    = { { 1, 128, 64, 0 },  { 63, 53, 33, 0 }, { 32, 15, 100, 0 },
-        { 15, 32, 100, 0 }, { 1, 128, 64, 1 }, { 63, 53, 33, 1 },
-        { 32, 15, 100, 1 }, { 15, 32, 100, 1 } };
+std::vector<TestTransposeParams> params = {
+  { 1, 128, 64, 1 }, { 63, 53, 33, 1 }, { 32, 15, 100, 1 }, { 15, 32, 100, 1 }
+};
 
 INSTANTIATE_TEST_SUITE_P (test_transpose, TestTranspose,
                           ::testing::ValuesIn (params));
