fd3846fee9dd1400aa4a0cf67709da9dc11522e6
Conley Lee <conleylee@foxmail.com>
2024-11-24 18:07:25 +0800

    fix matmul

--- src/ops/mat_mul.cpp
+++ src/ops/mat_mul.cpp
@@ -68,6 +68,18 @@ MatMul::init () noexcept
           code_size                                                              \
               = __get_matmul_broadcast##__boradcast##_fp16a_v2_comp_spv_size (); \
         }                                                                        \
+      else if (a_dtype_ == FP16 && b_dtype_ == FP16 && transpose_b_              \
+               && dev_->support_fp16_arithmetic () && broadcast_type_ == 0)      \
+        {                                                                        \
+          pcode = __get_matmul_b0_tb_fp16a_v2_comp_spv_code ();                  \
+          code_size = __get_matmul_b0_tb_fp16a_v2_comp_spv_size ();              \
+        }                                                                        \
+      else if (a_dtype_ == FP16 && b_dtype_ == FP16 && transpose_b_              \
+               && broadcast_type_ == 0)                                          \
+        {                                                                        \
+          pcode = __get_matmul_b0_tb_fp16_v2_comp_spv_code ();                   \
+          code_size = __get_matmul_b0_tb_fp16_v2_comp_spv_size ();               \
+        }                                                                        \
       else if (a_dtype_ == FP16 && b_dtype_ == FP16)                             \
         {                                                                        \
           pcode                                                                  \
@@ -176,9 +188,15 @@ MatMul::operator() (Tensor a) noexcept
   int channels = std::max (a.channels (), weight_.channels ());
 
   uint32_t groupx = out_w, groupy = a.height (), groupz = channels;
-  if (a_dtype_ == FP16 && b_dtype_ == Q8_0)
+  if (a_dtype_ == FP16 && b_dtype_ == Q8_0 && broadcast_type_ == 0
+      && transpose_b_)
     {
       groupx = (out_w + Q8_0_TILE_X_SIZE - 1) / Q8_0_TILE_X_SIZE;
+    }
+  else if (a_dtype_ == FP16 && b_dtype_ == FP16 && transpose_b_
+           && broadcast_type_ == 0)
+    {
+      groupx = (out_w + FP16_TILE_X_SIZE - 1) / FP16_TILE_X_SIZE;
     }
 
   if (auto ret = pipeline_->set_group (groupx, groupy, groupz); !ret.ok ())
@@ -234,9 +252,15 @@ MatMul::operator() (Tensor a, Tensor b) noexcept
 
   uint32_t groupx = out_w, groupy = a.height (), groupz = channels;
 
-  if (a_dtype_ == FP16 && b_dtype_ == Q8_0)
+  if (a_dtype_ == FP16 && b_dtype_ == Q8_0 && transpose_b_
+      && broadcast_type_ == 0)
     {
       groupx = (out_w + Q8_0_TILE_X_SIZE - 1) / Q8_0_TILE_X_SIZE;
+    }
+  else if (a_dtype_ == FP16 && b_dtype_ == FP16 && transpose_b_
+           && broadcast_type_ == 0)
+    {
+      groupx = (out_w + FP16_TILE_X_SIZE - 1) / FP16_TILE_X_SIZE;
     }
 
   auto s = pipeline_->set_group (groupx, groupy, groupz);
